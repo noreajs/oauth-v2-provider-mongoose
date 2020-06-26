e24327a2f8402fe41b4aa1136bc131cb9a759e11
Arnold LAMBOU <lambouarnold@gmail.com>
2020-06-26 15:23:23 +0100

    Oauth.verifyToken method has been added

--- lib/module/Oauth.ts
+++ lib/module/Oauth.ts
@@ -9,6 +9,38 @@ import OauthAccessToken from "./models/OauthAccessToken";
 import { HttpStatus, replaceAllMatch } from "@noreajs/common";
 
 export default class Oauth {
+  static ERRORS = {
+    /**
+     * Access Token not approved
+     */
+    TOKEN_NOT_APPROVED: "Access Token not approved",
+
+    /**
+     * Insufficient scope
+     */
+    INSUFFICIENT_SCOPE: "Insufficient scope",
+
+    /**
+     * Invalid access token
+     */
+    INVALID_TOKEN: "Invalid access token",
+
+    /**
+     * Access Token expired
+     */
+    TOKEN_EXPIRED: "Access Token expired",
+
+    /**
+     * No Oauth instance running on this server
+     */
+    NO_INSTANCE: "No Oauth instance running on this server",
+
+    /**
+     * Authorization Header Required
+     */
+    AUTH_HEADER_REQUIRED: "Authorization Header Required",
+  };
+
   private static instance: Oauth;
   context: OauthContext;
   app: Application;
@@ -69,6 +101,82 @@ export default class Oauth {
   }
 
   /**
+   * Verify an access token
+   * @param token access token without the token type (Bearer, Token or everything else)
+   * @param success success callback
+   * @param error error callback which is a method with two parameters, reason and authError. authError = true means that the error is related to the token and false if it is and internal probleme
+   * @param scope scope to be checked in the access token
+   */
+  static async verifyToken(
+    token: string,
+    success: (userId: string, lookupData?: any) => Promise<void> | void,
+    error: (reason: string, authError: boolean) => Promise<void> | void,
+    scope?: string
+  ) {
+    // get auth instance
+    const oauth = Oauth.getInstance();
+    // check instance existance
+    if (oauth) {
+      // get oauth context
+      const oauthContext = oauth.context;
+      try {
+        // Verify token signature
+        const tokenData = jwt.verify(token, oauthContext.secretKey, {
+          algorithms: [oauthContext.jwtAlgorithm],
+        }) as IJwtTokenPayload;
+
+        // load access token
+        const accessToken = await OauthAccessToken.findById(tokenData.jti);
+
+        // access token must exist localy
+        if (accessToken) {
+          // revocation state
+          if (accessToken.revokedAt) {
+            error(Oauth.ERRORS.TOKEN_NOT_APPROVED, true);
+          } else {
+            // lookup sub for other grant but client_credentials
+            let user = undefined;
+            if (
+              accessToken.grant !== "client_credentials" &&
+              oauthContext.subLookup
+            ) {
+              user = await oauthContext.subLookup(accessToken.userId);
+            }
+
+            // scope validation if exist
+            if (scope) {
+              const tokenScope = tokenData.scope ?? accessToken.scope;
+              const tokenScopeParts = tokenScope.split(" ");
+              const scopeParts = scope.split(" ");
+              for (const item of tokenScopeParts) {
+                if (!scopeParts.includes(item)) {
+                  error(Oauth.ERRORS.INSUFFICIENT_SCOPE, true);
+                }
+              }
+
+              // the user can access to the resource
+              success(accessToken.userId, user);
+            } else {
+              // the user can access to the resource
+              success(accessToken.userId, user);
+            }
+          }
+        } else {
+          error(Oauth.ERRORS.INVALID_TOKEN, true);
+        }
+      } catch (err) {
+        error(Oauth.ERRORS.TOKEN_EXPIRED, true);
+      }
+    } else {
+      console.warn(
+        "The Oauth.context static property is not defined. Make sure you have initialized the Oauth package as described in the documentation."
+      );
+      // the user can access to the resource
+      error(Oauth.ERRORS.NO_INSTANCE, false);
+    }
+  }
+
+  /**
    * Validate user access token
    * @param scope scope needed - Optional
    */
@@ -90,62 +198,30 @@ export default class Oauth {
             // token parts
             const parts = authorization.split(" ");
             try {
-              // Verify token signature
-              const tokenData = jwt.verify(parts[1], oauthContext.secretKey, {
-                algorithms: [oauthContext.jwtAlgorithm],
-              }) as IJwtTokenPayload;
-
-              // load access token
-              const accessToken = await OauthAccessToken.findById(
-                tokenData.jti
-              );
-
-              // access token must exist localy
-              if (accessToken) {
-                // revocation state
-                if (accessToken.revokedAt) {
-                  return res.status(HttpStatus.Unauthorized).json({
-                    message: "Access Token not approved",
-                  });
-                } else {
-                  // lookup sub for other grant but client_credentials
-                  if (
-                    accessToken.grant !== "client_credentials" &&
-                    oauthContext.subLookup
-                  ) {
-                    res.locals.user = await oauthContext.subLookup(
-                      accessToken.userId
-                    );
-                  }
-
-                  // scope validation if exist
-                  if (scope) {
-                    const tokenScope = tokenData.scope ?? accessToken.scope;
-                    const tokenScopeParts = tokenScope.split(" ");
-                    const scopeParts = scope.split(" ");
-                    for (const item of tokenScopeParts) {
-                      if (!scopeParts.includes(item)) {
-                        return res.status(HttpStatus.Unauthorized).json({
-                          message: "Insufficient scope",
-                        });
-                      }
-                    }
-
-                    // the user can access to the resource
-                    next();
+              // verify the token
+              await Oauth.verifyToken(
+                parts[1],
+                (userId, user) => {
+                  res.locals.user = user;
+                  // continue
+                  next();
+                },
+                (reason: string, authError: boolean) => {
+                  if (authError) {
+                    res.status(HttpStatus.Unauthorized).json({
+                      message: reason,
+                    });
+                    return;
                   } else {
-                    // the user can access to the resource
+                    // continue
                     next();
                   }
-                }
-              } else {
-                return res.status(HttpStatus.Unauthorized).json({
-                  message: "Invalid access token",
-                });
-              }
+                },
+                scope
+              );
             } catch (error) {
               return res.status(HttpStatus.Unauthorized).json({
-                message: "Access Token expired",
+                message: Oauth.ERRORS.TOKEN_EXPIRED,
               });
             }
           } else {
@@ -155,7 +231,7 @@ export default class Oauth {
           }
         } else {
           return res.status(HttpStatus.Unauthorized).json({
-            message: `Authorization Header Required`,
+            message: Oauth.ERRORS.AUTH_HEADER_REQUIRED,
           });
         }
       } else {
