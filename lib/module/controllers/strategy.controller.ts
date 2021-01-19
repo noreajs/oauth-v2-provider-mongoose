b60adc00028ade509e555527593c7f0ac30bc753
Arnold LAMBOU <lambouarnold@gmail.com>
2021-01-19 17:32:11 +0100

    unused code removed in strategy controller

--- lib/module/controllers/strategy.controller.ts
+++ lib/module/controllers/strategy.controller.ts
@@ -388,22 +388,6 @@ class StrategyController extends OauthController {
         )
       );
     } else {
-      // // add error
-      // if (req.session) {
-      //   req.session.error = {
-      //     message: `No account is associated with your ${`${
-      //       strategy.options.providerName ?? strategy.options.identifier
-      //     }`.toLowerCase()} profile.`,
-      //   };
-      // }
-
-      // return res.redirect(
-      //   HttpStatus.TemporaryRedirect,
-      //   `${UrlHelper.getFullUrl(req)}/${
-      //     AuthorizationController.OAUTH_DIALOG_PATH
-      //   }`
-      // );
-
       return OauthHelper.throwError(
         req,
         res,
